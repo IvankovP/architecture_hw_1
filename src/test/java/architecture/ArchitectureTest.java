b0c635f701617a1272f9a251e7f3bd19fed84b39
20002233 <PSIvankov@sberbank.ru>
2023-03-19 22:08:24 +0300

    init

---
+++ src/test/java/architecture/ArchitectureTest.java
@@ -0,0 +1,14 @@
+package architecture;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ArchitectureTest {
+
+    @Test
+    void test() {
+        int a = 64;
+        assertEquals(64, a);
+    }
+}
